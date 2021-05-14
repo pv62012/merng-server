3069a4a2e9d8db09e371670ce9b61c5ae20f0f4a
pankaj verma <pv62012@gmail.com>
2021-05-14 21:41:45 +0530

    fsf

---
+++ config.js
@@ -0,0 +1,6 @@
+const MONGODB =
+  "mongodb+srv://admin:pankaj123@cluster0.rlo6w.mongodb.net/merng?retryWrites=true&w=majority";
+
+const SECRET_KEY = "some very secret hide it"
+ 
+export { MONGODB, SECRET_KEY };
---
+++ index.js
@@ -0,0 +1,32 @@
+import {ApolloServer, PubSub} from 'apollo-server'
+
+import mongoose from 'mongoose'
+
+
+import {MONGODB} from './config.js'
+import typeDefs from './graphql/typeDefs.js'
+import resolvers from './graphql/resolvers/index.js'
+
+const pubsub = new PubSub();
+
+const PORT = process.env.port || 5000
+
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({req})=>({req, pubsub})
+});
+
+mongoose
+    .connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology:true })
+    .then(() => {
+        console.log("DB Connected");
+      return server.listen({port:PORT})
+    })
+.then((res) => {
+      console.log(`server running at ${res.url} `);
+})
+    .catch((err) => {
+      console.error(err);
+  })
